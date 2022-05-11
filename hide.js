90d58ebbbc5b2cc93b197d3526c87fe81de1396b
hasnaazizah <zainhana249@gmail.com>
2022-05-11 07:05:06 +0700

    add projects

---
+++ hide.js
@@ -0,0 +1,39 @@
+// function clicked() {
+//     if (img == 1) {
+//       document.getElementById(
+//         "img"
+//       ).style.display = "inline";
+//       return img == 0;
+//     } else {
+//       document.getElementById(
+//         "img"
+//       ).style.display = "none";
+//       return img == 1;
+//     }
+//   }
+
+function clicked(){
+    var x = document.getElementById("img");
+
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+
+}
+
+function clicked2(){
+    var x = document.getElementById("img");
+
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+
+}
---
+++ hideshow.js
@@ -0,0 +1,80 @@
+// function clicked() {
+//     if (img == 1) {
+//       document.getElementById(
+//         "img"
+//       ).style.display = "inline";
+//       return img == 0;
+//     } else {
+//       document.getElementById(
+//         "img"
+//       ).style.display = "none";
+//       return img == 1;
+//     }
+//   }
+
+function clicked(){
+    var x = document.getElementById("img");
+
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+
+
+
+}
+
+function klik(){
+    var y = document.getElementById("img2");
+
+    if(y.style.display === "none"){
+        y.style.display = "block";
+    }
+
+    else{
+        y.style.display = "none";
+    }
+
+}
+
+function kluk(){
+    var y = document.getElementById("img3");
+
+    if(y.style.display === "none"){
+        y.style.display = "block";
+    }
+
+    else{
+        y.style.display = "none";
+    }
+
+}
+
+function klek(){
+    var y = document.getElementById("img4");
+
+    if(y.style.display === "none"){
+        y.style.display = "block";
+    }
+
+    else{
+        y.style.display = "none";
+    }
+}
+
+
+function ais(){
+    var y = document.getElementById("hihi");
+
+    if(y.style.display === "none"){
+        y.style.display = "block";
+    }
+
+    else{
+        y.style.display = "none";
+    }
+
+}
